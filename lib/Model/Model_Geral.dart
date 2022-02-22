95458b36de07e5b481dfb0dd83b3298ace67f4e8
ryansilvaf <ryansilvaf@gmail.com>
2022-02-21 22:37:58 -0300

    alterando Pagina NovaTarefa

--- lib/Model/Model_Geral.dart
+++ lib/Model/Model_Geral.dart
@@ -103,6 +103,4 @@ class DeleteItem {
       }
     });
   }
-
-  getLogin() {}
 }
--- lib/View/paginaNovaTarefa.dart
+++ lib/View/paginaNovaTarefa.dart
@@ -30,63 +30,61 @@ class _NovaTarefaState extends State<NovaTarefa> {
                 key: _formkey,
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(
-                          height: 30,
-                          width: 30,
-                        ),
-                        Center(
-                          child: Container(
-                              alignment: AlignmentDirectional.topCenter,
-                              child: const Text(
-                                'Nova Tarefa',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              margin:
-                                  const EdgeInsets.fromLTRB(40, 40, 40, 50)),
-                        ),
-                        TextFormField(
-                          controller: _controlerTitle,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Preenchimento obrigatorio!';
-                            }
-                          },
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                              color: Color(0xff3101B9),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w400),
-                          decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: ('Título tarefa'),
-                            hintStyle: const TextStyle(
-                              color: Color(0xFF3101B9),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 20,
-                            ),
-                            border: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    color: Color(0xFFA901F7),
-                                    width: 5,
-                                    style: BorderStyle.solid),
-                                borderRadius: BorderRadius.circular(15)),
+                  child: SingleChildScrollView(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(
+                            height: 30,
+                            width: 30,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Expanded(
-                          child: SizedBox(
-                            height: 200,
+                          Center(
+                            child: Container(
+                                alignment: AlignmentDirectional.topCenter,
+                                child: const Text(
+                                  'Nova Tarefa',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                margin:
+                                    const EdgeInsets.fromLTRB(40, 40, 40, 50)),
+                          ),
+                          TextFormField(
+                            controller: _controlerTitle,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Preenchimento obrigatorio!';
+                              }
+                            },
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                                color: Color(0xff3101B9),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400),
+                            decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: ('Título tarefa'),
+                              hintStyle: const TextStyle(
+                                color: Color(0xFF3101B9),
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      color: Color(0xFFA901F7),
+                                      width: 5,
+                                      style: BorderStyle.solid),
+                                  borderRadius: BorderRadius.circular(15)),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            height: 255,
                             child: TextFormField(
                               keyboardType: TextInputType.multiline,
                               maxLines: 50,
@@ -117,84 +115,89 @@ class _NovaTarefaState extends State<NovaTarefa> {
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          alignment: AlignmentDirectional.center,
-                          child: mapaDeCores(),
-                          margin: const EdgeInsets.only(
-                              top: 10, left: 40, right: 40),
-                          height: 40,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(
-                                        Colors.transparent),
-                                    elevation: MaterialStateProperty.all(0),
-                                  ),
-                                  onPressed: () {
-                                    (Navigator.of(context).pop(value));
-                                    if (_formkey.currentState!.validate()) {}
-                                  },
-                                  child: const Icon(
-                                    Icons.close,
-                                    color: Colors.white,
-                                    size: 70,
-                                  )),
-                              const SizedBox(
-                                width: 80,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Container(
+                              alignment: AlignmentDirectional.topStart,
+                              child: mapaDeCores(),
+                              margin: const EdgeInsets.only(
+                                top: 10,
+                                left: 40,
+                                right: 40,
                               ),
-                              ElevatedButton(
-                                  style: ButtonStyle(
+                              height: 40,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                    style: ButtonStyle(
                                       backgroundColor:
                                           MaterialStateProperty.all(
                                               Colors.transparent),
-                                      elevation: MaterialStateProperty.all(0)),
-                                  onPressed: () {
-                                    String title = _controlerTitle.text;
-                                    String assignment =
-                                        _controlerAssignment.text;
-                                    if (title.isNotEmpty &&
-                                        assignment.isNotEmpty) {
-                                      controller.novaTarefa(
-                                        title,
-                                        assignment,
-                                        _colorTarefa,
-                                        () {
-                                          Navigator.of(context).pop(true);
-                                        },
-                                        () {
-                                          const snackBar = SnackBar(
-                                            backgroundColor: Colors.red,
-                                            content: Text(
-                                                'Sua tarefa não foi cadastrada'),
-                                          );
+                                      elevation: MaterialStateProperty.all(0),
+                                    ),
+                                    onPressed: () {
+                                      (Navigator.of(context).pop(value));
+                                      if (_formkey.currentState!.validate()) {}
+                                    },
+                                    child: const Icon(
+                                      Icons.close,
+                                      color: Colors.white,
+                                      size: 70,
+                                    )),
+                                const SizedBox(
+                                  width: 80,
+                                ),
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.transparent),
+                                        elevation:
+                                            MaterialStateProperty.all(0)),
+                                    onPressed: () {
+                                      String title = _controlerTitle.text;
+                                      String assignment =
+                                          _controlerAssignment.text;
+                                      if (title.isNotEmpty &&
+                                          assignment.isNotEmpty) {
+                                        controller.novaTarefa(
+                                          title,
+                                          assignment,
+                                          _colorTarefa,
+                                          () {
+                                            Navigator.of(context).pop(true);
+                                          },
+                                          () {
+                                            const snackBar = SnackBar(
+                                              backgroundColor: Colors.red,
+                                              content: Text(
+                                                  'Sua tarefa não foi cadastrada'),
+                                            );
 
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(snackBar);
-                                        },
-                                      );
-                                    }
-                                  },
-                                  child: const Icon(
-                                    Icons.check,
-                                    color: Colors.white,
-                                    size: 70,
-                                  ))
-                            ],
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(snackBar);
+                                          },
+                                        );
+                                      }
+                                    },
+                                    child: const Icon(
+                                      Icons.check,
+                                      color: Colors.white,
+                                      size: 70,
+                                    ))
+                              ],
+                            ),
                           ),
-                        ),
-                      ]),
+                        ]),
+                  ),
                 ),
               ),
               Container(
